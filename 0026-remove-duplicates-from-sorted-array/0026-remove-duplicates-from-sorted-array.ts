43bb2216cd7f69a69aadd53a42eb764d09b7e4da
James Tipping <50486384+James-Tipping@users.noreply.github.com>
2024-10-16 19:23:23 +0100

    Time: 75 ms (36.38%), Space: 52.8 MB (76.16%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -0,0 +1,13 @@
+function removeDuplicates(nums: number[]): number {
+    if (nums.length === 0) return 0;
+
+    let k = 1;
+
+    for (let i=1; i<nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k += 1;
+        }
+    }
+    return k
+}
